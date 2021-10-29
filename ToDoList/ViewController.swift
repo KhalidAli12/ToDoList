1134c93ec410aa1e6f213214a903bb94be76cb62
Khalid Alshammeri <khalid.Ali.med.s@tuwaiq.edu.sa>
2021-10-29 17:43:49 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by khalid ali on 23/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
